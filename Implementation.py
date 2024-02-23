f7c0d89d4309f494240c529dc135f11706f5b08b
RanxduG <ranidu.h.gurusinghe@gmail.com>
2024-02-23 19:40:55 +0530

    Added some styles to the page

--- Implementation.py
+++ Implementation.py
@@ -1,8 +1,11 @@
+# app.py
+
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import RandomForestClassificationModel
 import TimeSeriesModel
-
-from flask import Flask, render_template, request, jsonify
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 app = Flask(__name__, template_folder='Templates')
 
@@ -13,12 +16,9 @@ rf_selected_features = ['DaysWorked', 'DayOfWeek', 'Encoded Code', 'LeaveMonth',
                         'Reason_12', 'Reason_13', 'Reason_14', 'Reason_15', 'Reason_16', 'Reason_17',
                         'Reason_18']
 
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
     global updated_df
-    month = 10
-    year = 2023
     if request.method == 'GET':
         return render_template('EAPSPage.html')
 
@@ -36,8 +36,7 @@ def main():
         except Exception as e:
             return f'Error reading Excel file: {e}', 400
 
-        ts_model = TimeSeriesModel.load_model(
-            'C:/Ranidu/University/2nd Year/2nd Year/Semester 1/DSGP/Model/arima_model.pkl')
+        ts_model = TimeSeriesModel.load_model('C:/Ranidu/University/2nd Year/2nd Year/Semester 1/DSGP/Model/arima_model.pkl')
         forecast = TimeSeriesModel.get_time_series_forecast(ts_model, 3)
         print(forecast[22])
         if df['LeaveYear'][0] == 2023 and df['LeaveMonth'][0] == 9:
@@ -49,18 +48,27 @@ def main():
 
         print("ARIMA Forecast done")
         df_selected = RandomForestClassificationModel.get_features(updated_df, rf_selected_features)
-        rf_model = RandomForestClassificationModel.load_model(
-            'C:/Ranidu/University/2nd Year/2nd Year/Semester 1/DSGP/Model/rf_model.pkl')
+        rf_model = RandomForestClassificationModel.load_model('C:/Ranidu/University/2nd Year/2nd Year/Semester 1/DSGP/Model/rf_model.pkl')
         predictions = RandomForestClassificationModel.predict(rf_model, df_selected)
 
-        employee_codes = RandomForestClassificationModel.get_high_prob_employee_codes(rf_model, df_selected,
-                                                                                      predictions)
+        employee_codes = RandomForestClassificationModel.get_high_prob_employee_codes(rf_model, df_selected, predictions)
         predictions_list = list(employee_codes)
-        response = {
-            'predictions': predictions_list
-        }
 
-        return jsonify(response)
+
+        leave_reason_counts = df['Reason'].value_counts()
+
+        # Create bar plot for leave reasons
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=leave_reason_counts.index, y=leave_reason_counts.values)
+        plt.title('Leave Reasons')
+        plt.xlabel('Reason')
+        plt.ylabel('Number of Leaves')
+        plt.xticks(rotation=45)
+        plt.tight_layout()  # Adjust layout to prevent overlapping labels
+        leave_reason_plot_path = 'Images/leave_reason_plot.png'
+        plt.savefig(leave_reason_plot_path)
+
+        return render_template('EAPSPage.html', predictions=predictions_list)
 
 
 if __name__ == '__main__':
